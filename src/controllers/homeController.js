42443053b85619e7f5535d58a9be2b79b34833cf
Alexander Uzunov <kaialexander.11@gmail.com>
2023-10-16 12:19:23 +0300

    Add cube details page

--- src/controllers/homeController.js
+++ src/controllers/homeController.js
@@ -21,4 +21,5 @@ router.get('/about', (req, res) => {
     res.render('about');
 });
 
+
 module.exports = router;
--- src/managers/cubeManager.js
+++ src/managers/cubeManager.js
@@ -1,9 +1,34 @@
 const uniqid = require('uniqid');
 //const db = require('../db.json');
 
-const cubes = [];
+const cubes = [
+    {
+        id: '1nbo9fv4lnsnl1f2',
+        name: 'Machine Cube',
+        description: 'This cube creates Robots.',
+        imageUrl: 'https://cdnb.artstation.com/p/assets/images/images/016/833/323/large/michail-m-panopticum.jpg?1553640514',
+        difficultyLevel: 4
+    },
+
+    {
+        id: '11bo9fv4lnsnl222',
+        name: 'Multiverse Cube',
+        description: 'This cube opens portals between Universes.',
+        imageUrl: 'https://cdnb.artstation.com/p/assets/images/images/051/319/823/large/andrej-z-t-september-17.jpg?1657011503',
+        difficultyLevel: 4
+    },
+
+    {
+        id: '11bo5554lnsnl888',
+        name: 'Cyber Cube',
+        description: 'This cube makes the fastest calculations.',
+        imageUrl: 'https://cdna.artstation.com/p/assets/images/images/037/252/544/4k/samuel-speziali-2k.jpg?1619894238',
+        difficultyLevel: 4
+    },
+];
 
 exports.getAll = () => cubes.slice();
+exports.getOne = (cubeId) => cubes.find(x => x.id == cubeId); 
 
 exports.create = (cubeData) => {
 
