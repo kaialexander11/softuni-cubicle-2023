6fbe19ea105192721ea4d29c528f806bcd2312ae
Alexander Uzunov <kaialexander.11@gmail.com>
2023-10-20 17:23:25 +0300

    Setup mongoose

---
+++ src/config/dbConfig.js
@@ -0,0 +1,11 @@
+const mongoose = require('mongoose');
+
+const uri = 'mongodb://127.0.0.1:27017/cubicle-may-2023'
+
+async function dbConnect() {
+    await mongoose.connect(uri);
+}
+
+dbConnect()
+
+module.exports = dbConnect;
--- src/index.js
+++ src/index.js
@@ -1,30 +1,36 @@
-//console.log('Hello world!');
-
 const express = require('express');
-//const handlebars = require('express-handlebars');
-//const path = require('path');
 
+const dbConnect = require('./config/dbConfig');
 const expressConfig = require('./config/expressConfig.js');
 const handlebarsConfig = require('./config/handlebarsConfig.js');
 const routes = require('./routes');
-
 
 const app = express();
 
 const PORT = 5000;
 
-// Express config:
-
 expressConfig(app);
+handlebarsConfig(app);
+
+dbConnect()
+    .then(() => console.log('DB Connected successfully!'))
+    .catch(err => {
+        console.log('DB error: ', err);
+    });
+
+app.use(routes);
+app.listen(PORT, () => console.log(`Server is running on port ${PORT}... `));
+
+
+//console.log('Hello world!');
+//const handlebars = require('express-handlebars');
+//const path = require('path');
+// Express config:
 // require('./config/expressConfig')(app); => valid and does the same as the line above! (optional)
 //app.use(express.static('src/public')); => This will work exactly as the line below!
 //app.use(express.static(path.resolve(__dirname, 'public')));
 
 //Handlebars:
-handlebarsConfig(app);
-app.use(routes);
 
 //HOME ROUTE
 //app.get('/', homeController.getHome);
-
-app.listen(PORT, () => console.log(`Server is running on port ${PORT}... `));
