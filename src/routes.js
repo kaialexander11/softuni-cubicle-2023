552e5708330cf5a2cc60b01191f527af717fe0ca
Alexander Uzunov <kaialexander.11@gmail.com>
2023-10-21 11:28:50 +0300

    Add accessory page

--- src/routes.js
+++ src/routes.js
@@ -2,10 +2,12 @@ const router = require('express').Router();
 
 const homeController = require('./controllers/homeController.js');
 const cubeController = require('./controllers/cubeController.js');
+const accessoryController = require('./controllers/accessoryController');
 
 
 router.use(homeController);
 router.use('/cubes', cubeController);
+router.use('/accessories', accessoryController);
 router.get('*', (req, res) => {
     //res.render(404);
     res.redirect('/404');
