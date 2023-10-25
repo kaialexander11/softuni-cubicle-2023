f4e582ec1643ef7fd3c0231268116808b037193b
Alexander Uzunov <kaialexander.11@gmail.com>
2023-10-25 20:43:28 +0300

    Show buttons conditionally

--- src/controllers/cubeController.js
+++ src/controllers/cubeController.js
@@ -46,7 +46,12 @@ router.get('/:cubeId/details', async (req, res) => {
         return res.redirect('/404');
     }
 
-    res.render('cube/details', { cube });
+    const isOwner = cube.owner?.toString() === req.user._id;
+
+    // console.log(cube.owner.toString());
+    // console.log(req.user._id);
+    // console.log(isOwner);
+    res.render('cube/details', { cube, isOwner });
 });
 
 router.get('/:cubeId/attach-accessory', async (req, res) => {
